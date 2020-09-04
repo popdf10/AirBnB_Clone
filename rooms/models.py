fcd0761c16a804c27b070f82b1f6a6c6ec5d6bac
YuniL <popdf10@gmail.com>
2020-09-04 11:28:53 +0900

    Seeding

--- rooms/models.py
+++ rooms/models.py
@@ -53,7 +53,7 @@ class Photo(core_models.TimeStampedModel):
     """ Photo Model Definition """
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField(upload_to="room-photos")
+    file = models.ImageField(upload_to="room_photos")
     room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
@@ -97,11 +97,8 @@ class Room(core_models.TimeStampedModel):
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
-        for review in all_reviews:
-            all_ratings += review.rating_average()
-
-        divide = len(all_reviews)
-        if divide == 0:
-            divide = 1
-
-        return round(all_ratings / divide, 2)
+        if len(all_reviews) > 0:
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return round(all_ratings / len(all_reviews), 2)
+        return 0
